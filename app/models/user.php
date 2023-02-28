a27a694b5c3fb826213529d9fede88ff9a2cb5b2
= <=>
2023-02-28 16:48:05 +0700

    add

---
+++ app/models/user.php
@@ -0,0 +1,76 @@
+<?php
+
+class User
+{
+
+    function login($POST)
+    {
+        $DB = new Database();
+        $_SESSION['error'] = "";
+
+        if (isset($POST['username']) && isset($POST['password'])) {
+            $arr['username'] = $POST['username'];
+            $arr['password'] = $POST['password'];
+
+            $query = "SELECT * from users where users.name= :username && users.password = :password limit 1";
+
+            $data = $DB->read($query, $arr);
+            if (is_array($data)) {
+                //login
+                $_SESSION['user_id'] = $data[0]->userid;
+                $_SESSION['user_name'] = $data[0]->username;
+                $_SESSION['user_url'] = $data[0]->user_address;
+            } else {
+                $_SESSION['error'] = "Wrong username or password";
+            }
+
+        } else {
+            $_SESSION['error'] = "Please enter username or password";
+
+        }
+    }
+
+    function signup($POST)
+    {
+        $DB = new Database();
+        $_SESSION['error'] = "";
+
+        if (isset($POST['username']) && isset($POST['password'])&& isset($POST['email'])) {
+            $arr['username'] = $POST['username'];
+            $arr['password'] = $POST['password'];
+            $arr['email'] = $POST['email'];
+
+            $query = "INSERT INTO users (name, password, email) VALUES (:username,:password,:email)";
+
+            $data = $DB->write($query, $arr);
+            if($data){
+                header("Location:". ROOT. "login");
+                die;
+            }
+
+    }else{
+         $_SESSION['error'] = "Please enter a valid username or password"; 
+    }
+
+    function check_logged_in()
+    {
+        if (isset($_SESSION['user_url'])) {
+            $arr['user_url'] = $_SESSION['user_url'];
+
+            $query = "SELECT * from users where url_address= :user_url limit 1";
+
+            $data = $DB->read($query, $arr);
+            if (is_array($data)) {
+                //login
+                $_SESSION['user_id'] = $data[0]->userid;
+                $_SESSION['user_name'] = $data[0]->username;
+                $_SESSION['user_url'] = $data[0]->user_address;
+
+                return true;
+            } 
+        }
+        return false;   
+    }
+}
+
+?>
--- app/views/mini/login.php
+++ app/views/mini/login.php
@@ -103,16 +103,16 @@ body {
 <?php $this->view("mini/header", $data);?>  
 <div class="login-page">
   <div class="form">
-    <form class="register-form">
-      <input type="text" placeholder="name"/>
-      <input type="password" placeholder="password"/>
-      <input type="text" placeholder="email address"/>
+    <form class="register-form" method = "POST">
+      <input type="text" name="username" placeholder="name"/>
+      <input type="password" name="password" placeholder="password"/>
+      <input type="text" name="email" placeholder="email address"/>
       <button>create</button>
       <p class="message">Already registered? <a href="#">Sign In</a></p>
     </form>
     <form class="login-form">
-      <input type="text" placeholder="username"/>
-      <input type="password" placeholder="password"/>
+      <input type="text" name="username" placeholder="username"/>
+      <input type="password" name="password" placeholder="password"/>
       <button>login</button>
       <p class="message">Not registered? <a href="#">Create an account</a></p>
     </form>
